33b4f13e6d5a49edcaee98318700805bbda98782
tacyan <tatsuharase@firstserver.co.jp>
2015-08-20 19:01:38 +0900

    urlの変更

--- easy_talk.rb
+++ easy_talk.rb
@@ -37,15 +37,47 @@ class EasyTalk
     res = https.request(req)
     res.body
   end
+
+  def slack(utt,context="",t="20")
+    uri = URI.parse("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{ENV["APIKEY"]}")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["Content-Type"] = "application/json"
+
+    payload = {
+      utt: utt,
+      context: context,
+      t: t
+    }.to_json
+
+    req.body = payload
+    res = https.request(req)
+    res.body
+  end
+
+  def docomo(params)
+    uri = URI.parse("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{ENV["APIKEY"]}")
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["Content-Type"] = "application/json"
+
+    payload = {}.merge!(params).to_json
+
+    req.body = payload
+    res = https.request(req)
+    res.body
+  end
 end
 
-  post '/' do
+  post '/slack' do
     begin
       text = params[:text] ||= ""
       params[:user_id] ||= ""
       text.slice!(params[:trigger_word]) if params[:trigger_word]
       easy_tolk = EasyTalk.new
-      response = easy_tolk.post(text,params[:user_id])
+      response = easy_tolk.slack(text,params[:user_id])
       res = JSON.parse(response)
       { text: res["utt"] }.to_json
     rescue JSON::ParserError => e
@@ -55,11 +87,18 @@ end
     end
   end
 
+  post '/docomo' do
+    easy_tolk = EasyTalk.new
+    response = easy_tolk.docomo(params)
+    res = JSON.parse(response)
+    res.to_json
+  end
+
   get '/test' do
     erb :test
   end
 
-  get '/index' do
+  get '/' do
     erb :index
   end
 
